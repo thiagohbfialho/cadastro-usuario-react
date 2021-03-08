8856a8d69f462467e72cba3ac1b8be7f0767ff9a
Thiago Fialho <thiago.fialhoti@gmail.com>
2021-03-08 15:34:46 -0300

    Cadastro de Usuários com React

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capítulo de react" >
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para simplificação a construção
+    de um cadastro desenvolvido em React!</p>
+    </Main>
