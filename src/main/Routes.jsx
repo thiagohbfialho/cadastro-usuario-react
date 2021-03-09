8adecb74bbd00b491172061e6f8d96591b1bf5af
Thiago Fialho <thiago.fialhoti@gmail.com>
2021-03-09 15:50:26 -0300

    Adicionado a operação incluir

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -7,4 +7,6 @@ import UserCrud from '../components/user/UserCrud'
 export default props =>
     <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/users' component={UserCrud} />
+        <Redirect from='*' to='/' />
     </Switch>
